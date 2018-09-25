e73513f783710b31f03543f1d73147c3e3a070ab
Noel <oatcoder@gmail.com>
2018-09-25 13:16:32 -0500

    added jasmine-ts packages for unit test support.  created unit tests .

--- src/badWords.spec.ts
+++ src/badWords.spec.ts
@@ -4,12 +4,22 @@ import * as BadWord from './badWords';
 import * as Config from './config';
 
 
-describe('Bad Word', () => {
-    describe('hasBadWords function', () => {
-        it('should return true when a string does not have bad words', () => {
+describe('BadWords', () => {
+    describe('hasBadWords', () => {
+        beforeAll(() => {
             Config.default.setBadWords(['crap']);
+        });
 
+        afterAll(() => {
+            Config.default.setBadWords([]);
+        });
+
+        it('should return true when a string does not have bad words', () => {
             expect(BadWord.default.hasBadWords('Thats crap! said the brown bird')).toBe(true);
         });
+
+        it('should return false when a string contains bad words',  () => {
+            expect(BadWord.default.hasBadWords('The One With The Unagi')).toBe(false);
+        });
     })
-})
+});
