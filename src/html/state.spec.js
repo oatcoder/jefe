c4ade3b77cb6597d39c4fbd0381c4610debe1de8
noel <oatcoder@gmail.com>
2019-09-05 10:36:23 -0500

    fixed specs

--- src/html/state.spec.js
+++ src/html/state.spec.js
@@ -1,20 +1,20 @@
-const assert = require('assert')
 const { ElementVisibilityState } = require('./state')
 
 describe('ElementVisibilityState', () => {
   let rootHtmlStub
   beforeEach(() => {
     rootHtmlStub = document.createElement('div')
+    rootHtmlStub.innerHTML = `<div class="lang-grid" id="language">This is a sample</div>`
+    document.body.appendChild(rootHtmlStub)
+  })
+
+  it('should not show element', () => {
+    ElementVisibilityState(rootHtmlStub.children[0], false)
+    expect(rootHtmlStub.children[0].classList.contains('hidden'))
   })
 
   it('should show element', () => {
-    rootHtmlStub.innerHTML = `<div class="lang-grid" id="language">This is a sample</div>`
-    document.body.appendChild(rootHtmlStub)
-    ElementVisibilityState(rootHtmlStub.innerHTML, false)
-    expect(rootHtmlStub.innerHTML.classList.contains('hidden'))
+    ElementVisibilityState(rootHtmlStub.children[0], true)
+    expect(rootHtmlStub.children[0].classList.contains('hidden')).toBe(false)
   })
-
-  // it('should hide element', () => {
-  //
-  // })
 })
